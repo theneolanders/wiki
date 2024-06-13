fe4d2e000000c3a2b6f0c764d678661a03368ae9
alizardguy <coyotehelp123@gmail.com>
2024-06-12 19:24:03 -0700

    MY LIFE IS LIKE A VIDEO GAME
    testing font system

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "Schibsted Grotesk",
-        body: "Rubik",
+        body: "Tiny5",
         code: "IBM Plex Mono",
       },
       colors: {
