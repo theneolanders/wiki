64a2bcc2ec35e844803abb727b6d65ad4140e9ec
alizardguy <coyotehelp123@gmail.com>
2024-06-12 19:25:50 -0700

    back to Rubik

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "Schibsted Grotesk",
-        body: "Tiny5",
+        body: "Rubik",
         code: "IBM Plex Mono",
       },
       colors: {
